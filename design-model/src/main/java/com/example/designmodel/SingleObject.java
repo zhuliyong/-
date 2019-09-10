1fb564721b1a83e25e49ec43f5449265e9d6cabc
liyong.zhu <peter@vicmob.cn>
2019-09-10 16:30:12 +0800

    readme

--- design-model/src/main/java/com/example/designmodel/SingleObject.java
+++ design-model/src/main/java/com/example/designmodel/SingleObject.java
@@ -7,7 +7,7 @@ import java.util.Map;
 /**
  * 单例模式
  * 1、私有构造函数，为了不在其他地方初始化
- * 2、私有初始化
+ * 2、私有静态初始化
  * 3、提供外部调用的方法
  * @author peter
  * @version 1.1
@@ -21,26 +21,23 @@ public class SingleObject {
     private static SingleObject instance = new SingleObject(); //2、初始化
     public static SingleObject getInstance(){  //3、提供对外的入口
         return instance;
-    }
+    } //3、对外开发接口
+
+
+
 
     /* 定义可使用的方法 */
-    private static Map<String,Object> map = new HashMap<String, Object>();
-    void do1(String str){
+    public static Map<String,Object> map = new HashMap<String, Object>();
+    public void do1(String str){
         System.out.println(str);
         map.put(str,str);
     }
-    void do2(){
+    public void do2(){
         System.out.println("欢迎访问单例模式");
         System.out.println(map.toString());
     }
 
 
-    public static void main(String[] str){
-        SingleObject single = SingleObject.getInstance();
-        single.do1("wangw");
-        single.do1("xilou");
-        single.do2();
-    }
 
 
 }
---
+++ design-model/src/main/java/com/example/designmodel/controller/TestGlobleObject.java
@@ -0,0 +1,30 @@
+package com.example.designmodel.controller;
+
+import com.example.designmodel.SingleObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author peter
+ * @version 1.1
+ * @date 2019/9/10 15:32
+ */
+
+@Controller
+public class TestGlobleObject {
+
+    @RequestMapping("putmsg")
+    @ResponseBody
+    public void putMsg(HttpServletRequest req){
+        SingleObject.getInstance().do1(req.getParameter("name"));
+    }
+
+    @RequestMapping("getmsg")
+    @ResponseBody
+    public void getMsg(){
+        SingleObject.getInstance().do2();
+    }
+}
